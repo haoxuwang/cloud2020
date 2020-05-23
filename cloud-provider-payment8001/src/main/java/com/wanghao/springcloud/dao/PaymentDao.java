8daa73320fd4234b1a44164aa7b63080c0d1ff05
wanghao <87025569@qq.com>
2020-05-23 14:43:57 +0800

    init

---
+++ cloud-provider-payment8001/src/main/java/com/wanghao/springcloud/dao/PaymentDao.java
@@ -0,0 +1,16 @@
+package com.wanghao.springcloud.dao;
+import com.wanghao.springcloud.entities.Payment;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+/**
+ * @ClassName: PaymentDao
+ * @description: 支付持久层
+ * @author: XZQ
+ * @create: 2020/3/5 17:58
+ **/
+@Mapper
+public interface PaymentDao {
+    int create(Payment payment);
+    Payment getPaymentById(@Param("id") Long id);
+}
