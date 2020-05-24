ca70b91fdf064396151d1079c66cd5ce674c86e3
wanghao <87025569@qq.com>
2020-05-24 12:07:18 +0800

    provider8004 to zookeeper

---
+++ cloud-provider-payment8004/src/main/java/com/wanghao/springcloud/controller/PaymentController.java
@@ -0,0 +1,26 @@
+package com.wanghao.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+/**
+ * @auther wanghao
+ * @create 2020-05-24 11:15
+ */
+@RestController
+@Slf4j
+public class PaymentController
+{
+    @Value("${server.port}")
+    private String serverPort;
+
+    @RequestMapping(value = "/payment/zk")
+    public String paymentzk()
+    {
+        return "springcloud with zookeeper: "+serverPort+"\t"+ UUID.randomUUID().toString();
+    }
+}
